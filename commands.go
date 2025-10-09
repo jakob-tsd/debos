jakob-tsd/debos

commit af427ef092baac7ae1f7ce9063e0f58c9eb48b89

commands: systemd-nspawn: randomize machine name
When you have multiple debos chroot builds running at the same time,
you will get something like this:

	2025/09/16 11:17:38 apt | Mount point '/run/systemd/nspawn/unix-export/root' exists already, refusing.
	2025/09/16 11:17:38 Action `recipe` failed at stage Run, error: exit status 1

This because systemd-nspawn will name the container acc. to the root
directory per default, which is always "root" for debos, and wants
to create a subdir inside "/run/systemd/nspawn/unix-export" even
with "--register=no".

To reproduce this problem, you need two copies of a root file system where the
last component of the path has the same name.

The example below was run on Debian Trixie arm64 and uses
~/trixie1/trixie and ~/trixie2/trixie as the root directories:

	1st terminal:

	root@jaguar1:~/trixie1/trixie# systemd-nspawn --register=no bash
	░ Spawning container trixie on /root/trixie1/trixie.
	░ Press Ctrl-] three times within 1s to kill container.
	root@trixie:/#

	2nd terminal:

	root@jaguar1:~/trixie2/trixie# systemd-nspawn --register=no bash
	░ Spawning container trixie on /root/trixie2/trixie.
	░ Press Ctrl-] three times within 1s to kill container.
	Mount point '/run/systemd/nspawn/unix-export/trixie' exists already, refusing.
	# Try again
	root@jaguar1:~/trixie2/trixie# systemd-nspawn --register=no bash
	░ Spawning container trixie on /root/trixie2/trixie.
	░ Press Ctrl-] three times within 1s to kill container.
	Failed to allocate scope: Unit trixie.scope was already loaded or has a fragment file.

To fix this we problem, set the container name via "--machine"
to debos-NNNNN, where NNNNN is a random int63.

This makes name collisions virtually impossible and fixes the problem.

Signed-off-by: Jakob Unterwurzacher <jakob.unterwurzacher@cherry.de>

commands.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path"
@@ -231,6 +232,7 @@ func (cmd Command) Run(label string, cmdline ...string) error {
 		options = append(options, "--resolv-conf=off")
 		options = append(options, "--timezone=off")
 		options = append(options, "--register=no")
+		options = append(options, fmt.Sprintf("--machine=debos-%d", rand.Int63()))
 		options = append(options, "--keep-unit")
 		options = append(options, "--console=pipe")
 		for _, e := range cmd.extraEnv {

commit 8705007f72fc9246bcaba9f99532fa71e02327a3

commands: systemd-nspawn: randomize machine name using crypto.rand
I don't know how, but this happened today on one of our arm64 build boxes
that execute a lot of concurrent builds:

	2025/10/09 09:23:01 export DEBIAN_FRONTEND=noninteractive &&... | Mount point '/run/systemd/nspawn/unix-export/debos-3482892766072960174' exists already, refusing.
	2025/10/09 09:23:01 Action `recipe` failed at stage Run, error: exit status 1

Go version is 1.24.4 and debos version is af427ef092baac7ae1f7ce9063e0f58c9eb48b89.

I tried to get math.rand.Int63() to output duplicate values
using a toy program (and failed), but still, it seemed to have happend.

Switch to a 16-byte (=128-bit) value from crypto.Rand where this
definitely, positively, cannot happen.

This also fixes a problem for old Go versions:
Acc. to https://pkg.go.dev/math/rand#Seed, before Go 1.20,
you have to seed math.Rand manually, which I did not.

This change has been deployed to our three arm64 runners
on 2025-10-09 and I have not seen any problems so far.

Fixes: af427ef092baac7ae1f7ce9063e0f58c9eb48b89

commands.go
@@ -2,11 +2,11 @@ package debos
 
 import (
 	"bytes"
+	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
-	"math/rand"
 	"os"
 	"os/exec"
 	"path"
@@ -234,7 +234,9 @@ func (cmd Command) Run(label string, cmdline ...string) error {
 		options = append(options, "--resolv-conf=off")
 		options = append(options, "--timezone=off")
 		options = append(options, "--register=no")
-		options = append(options, fmt.Sprintf("--machine=debos-%d", rand.Int63()))
+		machineID := make([]byte, 16)
+		_, _ = rand.Read(machineID) // discard return values, this call never fails
+		options = append(options, fmt.Sprintf("--machine=debos-%x", machineID))
 		options = append(options, "--keep-unit")
 		options = append(options, "--console=pipe")
 		for _, e := range cmd.extraEnv {